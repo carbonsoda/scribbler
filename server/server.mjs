546ab9547bc83c30f7b59ca241881c8f70ad0d46
Avery Miller <carbonsoda@users.noreply.github.com>
2021-04-27 11:45:01 -0700

    Add basic server files

---
+++ server/server.mjs
@@ -0,0 +1,25 @@
+import express from "express";
+import mime from "mime-types";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+// By @gsong
+process.env?.SERVE_REACT?.toLowerCase() === "true" &&
+  app.use(
+    express.static("/app", {
+      maxAge: "1d",
+      setHeaders: (res, path) =>
+        ["application/json", "text/html"].includes(mime.lookup(path)) &&
+        res.setHeader("Cache-Control", "public, max-age=0"),
+    }),
+  );
+
+app.get("/api/ping", (req, res) =>
+  res.json({ res: "pong" }),
+);
+
+app.listen(port, () => {
+  console.info(`Server listening at http://localhost:${port}`);
+});
