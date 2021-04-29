34dd749af73b21e6e0697aafd7f054a77324d2c2
Avery Miller <carbonsoda@users.noreply.github.com>
2021-04-29 15:02:38 -0700

    Adjust prettier

--- app/.prettierrc.js
+++ app/.prettierrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   "printWidth": 80,
   "singleQuote": true,
+  "tabWidth": 2,
+  "useTabs": false,
+  "bracketSpacing": false,
+  "arrowParens": "avoid",
   "trailingComma": "all"
 }
