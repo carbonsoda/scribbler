105661ab2b7a2fa4aff157150a1269c1c7a77244
Avery Miller <carbonsoda@users.noreply.github.com>
2021-04-29 13:58:52 -0700

    Add 'Scribbler' as h1/title to navbar

--- app/src/components/navbar/Navbar.js
+++ app/src/components/navbar/Navbar.js
@@ -6,6 +6,7 @@ import UserIcon from './UserIcon';
 export default function Navbar() {
   return (
     <div className="navbar">
+      <h1>Scribbler</h1>
       <LoginBtn />
       <UserIcon />
     </div>
