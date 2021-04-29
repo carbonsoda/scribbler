775684c446a9c493831df5c68c775490a9d53fab
Avery Miller <carbonsoda@users.noreply.github.com>
2021-04-29 15:01:58 -0700

    Try implementing colormind.io API

--- app/src/components/sidebar/ColorPalette.js
+++ app/src/components/sidebar/ColorPalette.js
@@ -1,9 +1,29 @@
+/* eslint-disable no-console */
 import React from 'react';
 
 export default function ColorPalette() {
+  const [colorsHex, setColorsHex] = React.useState([]);
+
+  const generatePalette = async () => {
+    fetch('http://colormind.io/api/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ model: 'default' }),
+    })
+      .then((res) => res.json())
+      .then((results) => setColorsHex(results))
+      .then(() => console.log(colorsHex))
+      .catch((e) => console.error(e));
+  };
+
+  React.useEffect(() => generatePalette(), []);
+
   return (
     <div className="palette-generator">
-      Daily Color Palette
+      Daily Color Palette:
+      {colorsHex}
     </div>
-  )
+  );
 }
