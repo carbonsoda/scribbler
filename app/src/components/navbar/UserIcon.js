eade99efcfc46035a33481e64bb419db8fc79b04
Avery Miller <carbonsoda@users.noreply.github.com>
2021-04-29 12:46:03 -0700

    Add user icon component with skeleton code

---
+++ app/src/components/navbar/UserIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function UserIcon({ iconUrl }) {
+  return (
+    <div className="UserIcon">
+      {iconUrl ? <img src={iconUrl} alt="Current user's icon" /> : 'iconimg'}
+    </div>
+  );
+}
