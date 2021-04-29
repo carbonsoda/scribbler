85ba2bee6bcf19d82bab967dec803af2d371caa7
Avery Miller <carbonsoda@users.noreply.github.com>
2021-04-29 14:14:59 -0700

    Add buttons to sidebar

--- app/src/components/Sidebar.js
+++ app/src/components/Sidebar.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import DownloadBtn from './sidebar/DownloadBtn';
+import HistoryBtn from './sidebar/HistoryBtn';
+import ShareBtn from './sidebar/ShareBtn';
+
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      
+      <ShareBtn />
+      <DownloadBtn />
+      <HistoryBtn />
     </div>
   )
 }
