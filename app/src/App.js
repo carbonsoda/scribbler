dd36e910589d0c113c4151550d527fa414f977f1
Avery Miller <carbonsoda@users.noreply.github.com>
2021-04-29 13:55:16 -0700

    Create App.css and add it to App.js

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import DrawPanel from './components/DrawPanel';
 import Navbar from './components/navbar/Navbar';
 
+import './App.css';
+
 export default function App() {
   return (
     <div className="App">
