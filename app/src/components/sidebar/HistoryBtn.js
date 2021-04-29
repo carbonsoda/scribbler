ec1f02f9c0936080282a7b9b84bcdf0c376fabb4
Avery Miller <carbonsoda@users.noreply.github.com>
2021-04-29 14:12:47 -0700

    Create user history button component

---
+++ app/src/components/sidebar/HistoryBtn.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HistoryBtn() {
+  // TODO: configure react-router to appropiately set up this button, so routes to /history
+  return (
+    <button className="history-btn">
+      My upload history
+    </button>
+  )
+}
