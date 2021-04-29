dd537e61bbf032a710ea7b2686b8390fbf70f2c3
Avery Miller <carbonsoda@users.noreply.github.com>
2021-04-29 14:15:50 -0700

    Add sidebar to App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import DrawPanel from './components/DrawPanel';
+import Sidebar from './components/Sidebar';
 import Navbar from './components/navbar/Navbar';
 
 import './App.css';
@@ -11,6 +12,7 @@ export default function App() {
       <Navbar />
       <div className="main-page">
         <DrawPanel />
+        <Sidebar />
       </div>
     </div>
   );
