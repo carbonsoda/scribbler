0a3032797347a4e79788cbafb0053340662fc5b9
Avery Miller <carbonsoda@users.noreply.github.com>
2021-04-29 14:31:17 -0700

    Remove text from usericon when not logged in

--- app/src/components/navbar/UserIcon.js
+++ app/src/components/navbar/UserIcon.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function UserIcon({ iconUrl }) {
   return (
     <div className="UserIcon">
-      {iconUrl ? <img src={iconUrl} alt="Current user's icon" /> : 'iconimg'}
+      {iconUrl ? <img src={iconUrl} alt="Current user's icon" /> : ''}
     </div>
   );
 }
