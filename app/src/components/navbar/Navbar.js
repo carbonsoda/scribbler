5905ac15f0b452b33d54c6f14c0a6a70f08c30b4
Avery Miller <carbonsoda@users.noreply.github.com>
2021-04-30 15:09:47 -0700

    Add link to history and main page within navbar

--- app/src/components/navbar/Navbar.js
+++ app/src/components/navbar/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { Link } from '@reach/router';
+
 import LoginBtn from './LoginBtn';
 import UserIcon from './UserIcon';
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      <h1>Scribbler</h1>
+      <h1><Link to="/">Scribbler</Link></h1>
+      <Link to="/history">History</Link>
       <LoginBtn />
       <UserIcon />
     </div>
