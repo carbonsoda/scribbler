8626e3edb78cb69903e12c9ef4df253d11c43039
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-03 11:36:36 -0700

    Create useRef hook for the canvas

--- app/src/components/DrawPanel.js
+++ app/src/components/DrawPanel.js
@@ -3,10 +3,12 @@ import React from 'react';
 import CanvasDraw from 'react-canvas-draw';
 
 export default function DrawPanel() {
+  const canvas = React.useRef();
+
   return (
     <div className="draw-panel">
       <CanvasDraw
-        ref={activeCanvas}
+        ref={canvas}
         brushColor="rgba(155,12,60,0.7)"
       />
     </div>
