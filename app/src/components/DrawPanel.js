8eaed056e1473eed62cac2d3770115587d952a3e
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-03 11:07:22 -0700

    Insert react-draw-canvas into drawpanel component

--- app/src/components/DrawPanel.js
+++ app/src/components/DrawPanel.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import CanvasDraw from 'react-canvas-draw';
+
 export default function DrawPanel() {
   return (
     <div className="draw-panel">
-      Excalidraw would go here.
+      <CanvasDraw
+        ref={activeCanvas}
+        brushColor="rgba(155,12,60,0.7)"
+      />
     </div>
   );
 }
