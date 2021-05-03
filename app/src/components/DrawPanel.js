d76c402ad98ea762abc3ba23ec647e468fead28a
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-03 11:41:21 -0700

    Add Load btn which prints the dataURL of the user's canvas drawing

--- app/src/components/DrawPanel.js
+++ app/src/components/DrawPanel.js
@@ -5,10 +5,17 @@ import CanvasDraw from 'react-canvas-draw';
 export default function DrawPanel() {
   const canvas = React.useRef();
 
+  const getImg = () => {
+    // outputs a dataURL of the png with base64 encoding
+    // children[1] = the user's drawing layer, without background
+    console.log(canvas.current.canvasContainer.children[1].toDataURL('image/png'));
+  };
+
   return (
     <div className="draw-panel">
       <button onClick={() => canvas.current.clear()}>Clear</button>
       <button onClick={() => canvas.current.undo()}>Undo</button>
+      <button onClick={() => getImg()}>Load</button>
       <CanvasDraw
         ref={canvas}
         brushColor="rgba(155,12,60,0.7)"
