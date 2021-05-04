08caccd6d87e11bfa85d4a3ee0f2e1e1d4720b23
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-04 15:35:32 -0700

    Adjust radius of lazy-pointer

--- app/src/components/DrawPanel.js
+++ app/src/components/DrawPanel.js
@@ -9,7 +9,7 @@ export default function DrawPanel() {
   const canvas = React.useRef();
 
   // controls distance/length of the "lazy" pointer
-  const lazyRadius = 20;
+  const lazyRadius = 5;
   // states relating to brush tools
   const [brushSize, setBrushSize] = React.useState(10);
   const [brushColor, setBrushColor] = React.useState('rgba(0,0,0,1)');
