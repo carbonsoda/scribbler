aa4a5af5f01e6a7d83db013733a5ef5b218a3657
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-04 14:44:07 -0700

    Add clarifying comments for the downloadbtn

--- app/src/components/imgutils/DownloadBtn.js
+++ app/src/components/imgutils/DownloadBtn.js
@@ -4,11 +4,13 @@ export default function DownloadBtn({ canvas }) {
   const startDownload = (e) => {
     e.preventDefault();
 
-    const drawnlayer = canvas.current.canvasContainer.children[1].toDataURL('image/png');
+    // children[1] = the user's drawing layer, without background
+    const drawnLayer = canvas.current.canvasContainer.children[1].toDataURL('image/png');
 
+    // converts the base64-encoded png file so it can be downloaded as proper png
     // src: Ehsan Ahmadi https://stackoverflow.com/a/60719585
     const downloadLink = document.createElement('a');
-    downloadLink.href = drawnlayer;
+    downloadLink.href = drawnLayer;
     downloadLink.download = 'scribbled-img.png';
     downloadLink.click();
   };
