58ed64e9df253e0aeea85f861a4c25d97675a571
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-05 14:48:55 -0700

    Add image as express router and can respond to apiClient's requests

--- server/server.mjs
+++ server/server.mjs
@@ -1,9 +1,21 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-unused-expressions */
 import express from 'express';
 import mime from 'mime-types';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+const images = express.Router();
+images.use(express.json());
+
+images.post('/', async (req, res) => {
+  const { imgDataURL } = req.body;
+
+  res.json({ hi: 'there' });
+});
+
+app.use('/api/upload', images);
 
 // By @gsong
 process.env?.SERVE_REACT?.toLowerCase() === 'true'
