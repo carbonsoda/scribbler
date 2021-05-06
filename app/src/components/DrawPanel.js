4cfedb30721c596fa785c6b18edf6034e797b88b
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-05 21:34:27 -0700

    Seperate the actual canvas component from the general draw-panel/container

--- app/src/components/DrawPanel.js
+++ app/src/components/DrawPanel.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-import CanvasDraw from 'react-canvas-draw';
-
 import BrushTools from './drawutils/BrushTools';
+import CanvasPanel from './drawutils/CanvasPanel';
 import ImgUtils from './imgutils/ImgUtils';
 
 export default function DrawPanel() {
   const canvas = React.useRef();
 
-  // controls distance/length of the "lazy" pointer
-  const lazyRadius = 5;
   // states relating to brush tools
   const [brushSize, setBrushSize] = React.useState(10);
   const [brushColor, setBrushColor] = React.useState('rgba(0,0,0,1)');
@@ -17,14 +14,7 @@ export default function DrawPanel() {
   return (
     <div className="draw-panel">
       <ImgUtils canvas={canvas} />
-      <div className="canvas-draw">
-        <CanvasDraw
-          ref={canvas}
-          brushColor={brushColor}
-          lazyRadius={lazyRadius}
-          brushRadius={brushSize}
-        />
-      </div>
+      <CanvasPanel canvas={canvas} brushSize={brushSize} brushColor={brushColor} />
       <BrushTools onColorChange={setBrushColor} onSizeChange={setBrushSize} />
     </div>
   );
