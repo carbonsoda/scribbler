b77172db2d2def46423212aedfd33745446a7058
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-05 17:01:21 -0700

    Refactor code and eslint warning moved from entire file to just relevent line

--- server/server.mjs
+++ server/server.mjs
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-unused-expressions */
 import express from 'express';
 import mime from 'mime-types';
 
@@ -8,16 +6,18 @@ const port = process.env.PORT || 4000;
 
 const images = express.Router();
 images.use(express.json());
+app.use('/api/upload', images);
 
 images.post('/', async (req, res) => {
   const { imgDataURL } = req.body;
 
-  res.json({ hi: 'there' });
+  res.json({ hi: 'there', imgURL: imgDataURL.length });
 });
 
 app.use('/api/upload', images);
 
 // By @gsong
+// eslint-disable-next-line no-unused-expressions
 process.env?.SERVE_REACT?.toLowerCase() === 'true'
   && app.use(
     express.static('/app', {
