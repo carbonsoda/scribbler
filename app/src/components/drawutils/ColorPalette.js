8034d2c85b4819d593afe7c1d30ab2ee5ab19b6a
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-07 13:15:38 -0700

    Generated palette shows as a set of buttons

--- app/src/components/drawutils/ColorPalette.js
+++ app/src/components/drawutils/ColorPalette.js
@@ -9,14 +9,49 @@ export default function ColorPalette() {
   const generatePalette = async () => {
     // results array of 4 subarrays, each being an rgb-color
     const res = await apiClient.getColors();
-    // TODO: make into clickable swatches
-    setColors(`rgb(${res.join(') rgb(')})`);
+    setColors(res);
+  };
+
+  const swatchClick = (e, swatch) => {
+    e.preventDefault();
+    console.log(swatch);
   };
 
   return (
     <div className="palette-generator">
       <button onClick={generatePalette}>Generate a Color Palette</button>
-      {colors}
+      {
+        colors
+          ? (
+            <div className="palette-swatches">
+              {
+                colors.map((color) => (
+                  <button
+                    key={color}
+                    className="palette-swatch"
+                    style={{ background: `rgb(${color})` }}
+                    onClick={(e) => swatchClick(e, color)}
+                  />
+                ))
+              }
+            </div>
+          )
+          : ''
+      }
     </div>
   );
 }
+
+//
+// <div className="palette-swatches">
+//   {
+//     colors.map((color) => (
+//       <button
+//         key={color}
+//         className="palette-swatch"
+//         style={{ background: `rgb(${color})` }}
+//         onClick={(e) => swatchClick(e, color)}
+//       />
+//     ))
+//   }
+// </div>
