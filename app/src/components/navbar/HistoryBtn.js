2231f163d82e4de0ed7f5d6b7a7900abeee316e6
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-09 12:13:40 -0700

    Replace reach-router with react-router

--- app/src/components/navbar/HistoryBtn.js
+++ app/src/components/navbar/HistoryBtn.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-import { Link } from '@reach/router';
+import { Link } from 'react-router-dom';
 
 export default function HistoryBtn({ isAuthenticated }) {
   if (!isAuthenticated) {
+    // TODO: Show, but route to auth page instead
     return <></>;
   }
   return (
     <button className="history-btn">
-      <Link to="/history">Share History</Link>
+      <Link to="history">Share History</Link>
     </button>
   );
 }
