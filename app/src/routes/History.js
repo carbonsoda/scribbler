f8147d1bae468b92bcfde10f0eb711710e6edf61
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-09 18:11:12 -0700

    History page requires authentication before loading

--- app/src/routes/History.js
+++ app/src/routes/History.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { useAuth0 } from '@auth0/auth0-react';
+
+import Loading from '../components/Loading';
+
 export default function History() {
-  // TODO: make a protected route
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+
+  // Shows Loading component before redirecting
+  if (!isAuthenticated) {
+    loginWithRedirect();
+    return (<Loading />);
+  }
+
   return (
-    <div>
+    <div className="history-page">
       This is the history page.
     </div>
   );
