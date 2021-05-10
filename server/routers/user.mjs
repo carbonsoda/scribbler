09f537798ef0cfacb3961bfd0c81e0af45c67927
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-09 22:10:17 -0700

    Add route for potentially creating a user

--- server/routers/user.mjs
+++ server/routers/user.mjs
@@ -2,7 +2,7 @@
 import express from 'express';
 
 import checkJwt from '../check-jwt.mjs';
-// import * as db from '../db.mjs';
+import * as db from '../db.mjs';
 
 const userRouter = express.Router();
 userRouter.use(express.json());
@@ -13,6 +13,17 @@ userRouter.get('/public-message', (req, res) => {
 
 userRouter.get('/protected-message', checkJwt, (req, res) => {
   res.status(200).send({ message: 'The API successfully validated your access token.' });
+});
+
+userRouter.post('/create', async (req, res) => {
+  // check if user exists
+  const { email } = req.body;
+  const user = await db.getUser(email);
+
+  console.log(!user);
+  // TODO: if !user then add user
+
+  res.json({ user });
 });
 
 export default userRouter;
