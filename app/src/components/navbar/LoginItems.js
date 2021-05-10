8f46ec3c48106c9f7a55c006dffb39b41ea6fc33
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-09 22:17:25 -0700

    Temporarily add create/get user checking to the login items component

--- app/src/components/navbar/LoginItems.js
+++ app/src/components/navbar/LoginItems.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { useAuth0 } from '@auth0/auth0-react';
 
+import { createUser } from '../../apiClient';
+
 import HistoryBtn from './HistoryBtn';
 import LoginBtn from './LoginBtn';
 import UserIcon from './UserIcon';
@@ -15,8 +17,16 @@ export default function LoginItems() {
       logout({ returnTo: window.location.origin });
     } else {
       loginWithRedirect();
+      // TODO: as part of login process, check if new user
+      // ie Auth0 ManagementConsole can show login count with email
     }
   };
+
+  if (user) {
+    // TODO: remove after above's TODO
+    createUser(user);
+  }
+
   return (
     <div className="login-items">
       <HistoryBtn isAuthenticated={isAuthenticated} />
