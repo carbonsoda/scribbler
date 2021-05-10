51b8f4682afd2834495a750b70d101fa02720f82
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-09 19:41:29 -0700

    Add user's images component to history page

--- app/src/routes/History.js
+++ app/src/routes/History.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 import Loading from '../components/Loading';
+import UserImgs from '../components/UserImgs';
 
 export default function History() {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const {
+    isAuthenticated, loginWithRedirect,
+  } = useAuth0();
 
   // Shows Loading component before redirecting
   if (!isAuthenticated) {
@@ -16,6 +19,7 @@ export default function History() {
   return (
     <div className="history-page">
       This is the history page.
+      <UserImgs />
     </div>
   );
 }
