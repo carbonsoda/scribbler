fe6960caf0aca4ee266de55d5e77ecadacdfb416
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-10 12:24:09 -0700

    Lift the email_verified check into Navbar so a "verify email" message can render appropiately.

--- app/src/components/navbar/LoginItems.js
+++ app/src/components/navbar/LoginItems.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { useAuth0 } from '@auth0/auth0-react';
 
-import { createUser } from '../../apiClient';
-
 import HistoryBtn from './HistoryBtn';
 import LoginBtn from './LoginBtn';
 import UserIcon from './UserIcon';
@@ -12,6 +10,7 @@ export default function LoginItems() {
   const {
     loginWithRedirect, logout, isAuthenticated, user,
   } = useAuth0();
+
   const loginUser = () => {
     if (isAuthenticated) {
       logout({ returnTo: window.location.origin });
@@ -19,16 +18,6 @@ export default function LoginItems() {
       loginWithRedirect();
     }
   };
-
-  if (user) {
-    if (user.email_verified) {
-      // checks if user exists, else create it in db
-      createUser(user);
-    } else {
-      // TODO: If unverified, enable a banner/"please verify"
-    }
-    console.log(user);
-  }
 
   return (
     <div className="login-items">
