b30fcce0f2f7a3a8a3542f767233fba7cfc179b4
Avery Miller <carbonsoda@users.noreply.github.com>
2021-05-11 13:59:53 -0700

    Creating a user only returns user's id, not entire record

--- server/db.mjs
+++ server/db.mjs
@@ -8,7 +8,7 @@ export const getUser = async (user) => db.oneOrNone('SELECT * FROM users'
   + ' WHERE email=($1)', [user.email]);
 
 export const createUser = async (user) => db.one('INSERT INTO users(username, email)'
-  + ' VALUES ($1, $2) RETURNING *', [user.nickname, user.email]);
+  + ' VALUES ($1, $2) RETURNING user_id', [user.nickname, user.email]);
 
 export const getUserImages = async (user) => db.any(
   'SELECT img.img_id, img.img_url, img.time_created FROM images AS img'
