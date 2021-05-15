3a205d095193004d2001a907c94b30bfb2382e76
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-05-14 19:37:04 -0700

    History button will show regardless, to make the feature more obvious

--- app/src/components/navbar/HistoryBtn.js
+++ app/src/components/navbar/HistoryBtn.js
@@ -3,10 +3,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function HistoryBtn({ isAuthenticated }) {
-  if (!isAuthenticated) {
-    // Don't show if not authenticated
-    return <></>;
-  }
   return (
     <button className="history-btn">
       <Link to="/history">Share History</Link>
