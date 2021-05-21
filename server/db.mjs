2333eef3872cc118c135c5a6130248037e3cca83
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-05-20 23:02:21 -0700

    Add queries to get and update individual image entries

--- server/db.mjs
+++ server/db.mjs
@@ -26,6 +26,19 @@ export const addUserImage = async (id, fileName, shareUrl, url24Hr) => db.any(
   [id, fileName, url24Hr, new Date(), shareUrl, new Date()],
 );
 
+export const getUserImgUrl = async (id, fileName) => db.one(
+  'SELECT share_img_url, share_start_time FROM images'
+  + ' WHERE user_id=($1) AND img_name=($2)',
+  [id, fileName],
+);
+
+export const updateUserImageUrl = async (id, fileName, newUrl) => db.none(
+  'UPDATE images SET share_img_url=($1), share_start_time=($2)'
+  + ' WHERE user_id=($3) AND img_name=($4)'
+  + ' RETURNING share_start_time',
+  [newUrl, new Date(), id, fileName],
+);
+
 export const createUser = async (user) => db.oneOrNone(
   'INSERT INTO users(username, user_id)'
   + ' VALUES ($1, $2)'
