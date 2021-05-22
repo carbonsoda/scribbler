bd30481b4f93c50d452119e65ba08d2091fab1fa
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-05-22 14:47:17 -0700

    Adjust time display on card images

--- app/src/components/ImgCard.js
+++ app/src/components/ImgCard.js
@@ -8,12 +8,15 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import copy from 'clipboard-copy';
+import TimeAgo from 'timeago-react';
 
 import { renewShareUrl } from '../apiClient';
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    maxWidth: '25%',
     display: 'flex',
     flexDirection: 'column',
   },
@@ -26,25 +29,45 @@ const useStyles = makeStyles({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
+  text: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
 });
 
 export default function ImgCard({
   fileName, cardUrl, timeCreated, shareUrl, sharedAtTime, user,
 }) {
+  // TIME SETUP
+  // TODO: Make it so it says 0 min left for expired urls instead
+  const shareExpireTime = (startTime) => {
+    const current = new Date(startTime);
+    return new Date(current.getTime() + 30 * 60000);
+  };
+  const urlExpired = () => {
+    const diff = (expireTime.getTime() - Date.now()) / 60000;
+    return Math.abs(Math.round(diff)) >= 30;
+  };
+
+  // STATE HOOKS
   const [activeShareUrl, setActiveShareUrl] = React.useState(shareUrl);
-  const [activeUrlTime, setActiveUrlTime] = React.useState(sharedAtTime);
+  const [urlTimeCreated, setUrlTimeCreated] = React.useState(new Date(sharedAtTime));
+  const [expireTime, setExpireTime] = React.useState(shareExpireTime(urlTimeCreated));
 
   const refreshUrl = async (e) => {
     e.preventDefault();
 
-    if (user) {
+    if (user && urlExpired()) {
       const { url, time } = await renewShareUrl(user.sub, fileName);
       setActiveShareUrl(url);
-      setActiveUrlTime(time);
+      setUrlTimeCreated(new Date(time));
+      setExpireTime(shareExpireTime(time));
+      copy(url);
+    } if (user) {
+      copy(activeShareUrl);
     }
   };
 
-  // TODO: Stylize time display
   const classes = useStyles();
 
   return (
@@ -58,25 +81,26 @@ export default function ImgCard({
           image={cardUrl}
           title={fileName}
         />
-        <CardContent>
-          <Typography variant="body2" component="p">
-            Time created:
-            {timeCreated}
-          </Typography>
-          <Typography variant="body2" component="p">
-            Share it:
+        <CardContent className={classes.text}>
+          <Typography variant="subtitle1" component="p">
+            Scribbled this
             { ' ' }
-            <a href={activeShareUrl}>click</a>
+            <TimeAgo datetime={timeCreated} />
             <br />
-            Active since
-            {' '}
-            { activeUrlTime }
+            Link generated
+            { ' ' }
+            <TimeAgo datetime={sharedAtTime} />
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions className={classes.buttons}>
-        <Button size="medium" color="primary" onClick={(e) => refreshUrl(e)}>
-          Share
+        <Button
+          size="large"
+          color="primary"
+          onClick={(e) => refreshUrl(e)}
+          endIcon={<FileCopyIcon />}
+        >
+          Share your Scribble
         </Button>
       </CardActions>
     </Card>
