5da232ec2563d925b314e4930401b08f9dbdb546
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-05-22 14:49:08 -0700

    Add intro to home page

--- app/src/routes/Home.js
+++ app/src/routes/Home.js
@@ -5,6 +5,14 @@ import DrawPanel from '../components/DrawPanel';
 export default function Home() {
   return (
     <div className="main-page">
+      <p className="description">
+        Scribble an idea and quickly share it with others
+        via a temporary link
+        <br />
+        After 30 minutes, the link will expire.
+        <br />
+        Registered users can refresh the link for up to 24 hours.
+      </p>
       <DrawPanel />
     </div>
   );
