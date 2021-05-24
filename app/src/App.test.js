a04f153dcc360df205637cdb3fa8cee5e67e1e8c
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-05-23 23:22:10 -0700

    Try testing Loading component text

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -3,7 +3,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from './App';
+import Loading from './components/Loading';
 
 it('renders without crashing', () => {
   shallow(<App />);
+});
+
+it('renders without crashing', () => {
+  shallow(<Loading />);
+});
+
+it('renders Loading header', () => {
+  const wrapper = shallow(<Loading />);
+  const welcome = 'Loading...';
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
